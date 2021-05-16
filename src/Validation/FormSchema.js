9702f575eaaa5d0acd6602cbee198c77c5959f8b
Deborah Warren <Deborah.dw.32@gmail.com>
2021-05-16 19:47:37 -0400

    checking

--- src/Validation/FormSchema.js
+++ src/Validation/FormSchema.js
@@ -2,7 +2,7 @@ import * as yup from 'yup'
 
 export const FormSchema = yup.object().shape({
     name: yup.string().min(2, 'name must be at least 2 characters').required('Name is required!'),
-    pizzaSize: yup.string().oneOf(['small', 'medium', 'large', 'extraLarge'], 'Must select a size!')
+    pizzaSize: yup.string().oneOf(['small', 'medium', 'large', 'extraLarge'], 'Must select a size!').required('Pizza size is required!')
 })
 
 export default FormSchema
